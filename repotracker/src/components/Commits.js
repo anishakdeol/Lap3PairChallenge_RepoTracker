410b23907b3f1cff157414d6d39acd6456e03ec7
anishakdeol <97612531+anishakdeol@users.noreply.github.com>
2022-04-28 09:50:04 +0100

    bug fixed

--- repotracker/src/components/Commits.js
+++ repotracker/src/components/Commits.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+
     const Commits = ({ url }) => {
     const [commits, setCommits] = useState()
 
-
-    useEffect(async () => {
-
+    useEffect(()  => {
+        async function fetchData(){
+        // fetching commits from github url
         const options = {
             headers: {
                 Accept: "application/vnd.github.v3+json"
             }
         }
         const { data } = await axios.get(url.replace(/{\/sha}$/, ''), options);
-        
-        
         setCommits(data.length);
-    }, [url])
+    }}, [url])
 
     return (
         <>
             {commits}
         </>
     )
+    
 }
 
 export default Commits
