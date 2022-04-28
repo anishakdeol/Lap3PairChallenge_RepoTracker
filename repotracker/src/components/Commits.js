e501c3c487a5e782c2c4037811a9978409ddabac
anishakdeol <97612531+anishakdeol@users.noreply.github.com>
2022-04-28 09:40:59 +0100

    fixed commits

--- repotracker/src/components/Commits.js
+++ repotracker/src/components/Commits.js
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
     const Commits = ({ url }) => {
     const [commits, setCommits] = useState()
 
-    useEffect(()  => {
-        async function fetchData(){
-        // fetching commits from github url
+
+    useEffect(async () => {
+
         const options = {
             headers: {
                 Accept: "application/vnd.github.v3+json"
             }
         }
         const { data } = await axios.get(url.replace(/{\/sha}$/, ''), options);
+        
+        
         setCommits(data.length);
-    }}, [url])
+    }, [url])
 
     return (
         <>
